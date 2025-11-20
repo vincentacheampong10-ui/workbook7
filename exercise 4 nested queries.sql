82807e800f419a1109cd464feb84ae9aa245ac8b
vincentacheampong10-ui <vincentacheampong10@gmail.com>
2025-11-20 16:19:48 -0500

    Add files via upload

---
+++ exercise 4 nested queries.sql
@@ -0,0 +1,28 @@
+use sakila;
+
+SELECT 
+    customer_id,
+    first_name,
+    last_name,
+    customer.address_id AS customer_address_id,
+    address.address_id AS address_address_id,
+    address,
+    postal_code,
+    city_id
+FROM
+    customer
+	INNER JOIN address ON customer.address_id = address.address_id;
+    
+    
+ SELECT 
+    customer.customer_id,
+    customer.first_name,
+    customer.last_name,
+    address.address AS street_address,
+    address.postal_code,
+    address.city_id
+FROM customer
+INNER JOIN address 
+    ON customer.address_id = address.address_id;
+    
+ 
